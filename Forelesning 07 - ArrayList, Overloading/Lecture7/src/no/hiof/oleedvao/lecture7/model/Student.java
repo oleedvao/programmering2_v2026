e47aa6fc2d444fec747191fa760837eb45ef0baa
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2026-01-26 08:27:33 +0100

    Lecture 7 - ArrayList, Overloading - Pre lecture code

---
+++ Forelesning 07 - ArrayList, Overloading/Lecture7/src/no/hiof/oleedvao/lecture7/model/Student.java
@@ -0,0 +1,75 @@
+package no.hiof.oleedvao.lecture7.model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String studentId;
+    private Course favoriteCourse;
+
+    private String birthNumber = "12345678910";
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName,
+                   int age, String studentId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        setAge(age);
+        this.studentId = studentId;
+    }
+
+    public Course getFavoriteCourse() {
+        return favoriteCourse;
+    }
+
+    public void setFavoriteCourse(Course favoriteCourse) {
+        this.favoriteCourse = favoriteCourse;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            this.age = 0;
+        }
+        else if (age > 120) {
+            this.age = 120;
+        }
+        else {
+            this.age = age;
+        }
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getBirthNumber() {
+        return birthNumber;
+    }
+}
