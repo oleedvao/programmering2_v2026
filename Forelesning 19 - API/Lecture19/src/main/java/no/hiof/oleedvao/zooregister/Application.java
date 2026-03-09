6ead2cf3651e7b01ed332fd1b540d8775eee53f9
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2026-03-09 09:39:29 +0100

    Lecture 19 - Pre lecture project

---
+++ Forelesning 19 - API/Lecture19/src/main/java/no/hiof/oleedvao/zooregister/Application.java
@@ -0,0 +1,35 @@
+package no.hiof.oleedvao.zooregister;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create().start(1337);
+
+        app.get("/", new Handler() {
+            @Override
+            public void handle(@NotNull Context context) throws Exception {
+                context.result("Hello, Javalin!");
+            }
+        });
+
+        app.get("/other-page", new Handler() {
+            @Override
+            public void handle(@NotNull Context context) throws Exception {
+                context.result("Hello from the other page!");
+            }
+        });
+        
+        app.get("/some-page/page-in-page", new Handler() {
+            @Override
+            public void handle(@NotNull Context context) throws Exception {
+                context.result("This is a sub-page");
+            }
+        });
+
+    }
+}
