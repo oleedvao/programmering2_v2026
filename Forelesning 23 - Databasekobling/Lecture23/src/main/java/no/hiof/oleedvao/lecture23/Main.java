b8d926d03af16c615f262f16db2edae79041e148
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2026-03-20 12:22:57 +0100

    Lecture 23 - Database connection - Pre lecture code

---
+++ Forelesning 23 - Databasekobling/Lecture23/src/main/java/no/hiof/oleedvao/lecture23/Main.java
@@ -0,0 +1,8 @@
+package no.hiof.oleedvao.lecture23;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Forelesning 23 - Databasekobling/Lecture23/src/main/java/no/hiof/oleedvao/lecture23/model/Album.java
@@ -0,0 +1,47 @@
+package no.hiof.oleedvao.lecture23.model;
+
+public class Album implements Comparable<Album> {
+    private String title;
+    private String artist;
+    private int year;
+
+    public Album(String title, String artist, int year) {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+    }
+
+    @Override
+    public int compareTo(Album otherAlbum) {
+        return this.title.compareTo(otherAlbum.title);
+    }
+
+    @Override
+    public String toString() {
+        return title + " - " + artist + " - " + year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
